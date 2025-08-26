bb94a2a1ea2b5947eabfcee29da4d8c44ff091dc
Alexie Papanicolaou <alpapan@gmail.com>
2025-08-27 01:39:40 +1000

    name fix

--- rollup.config.js
+++ rollup.config.js
@@ -19,9 +19,27 @@ const includeCJS = stringToBoolean(process.env.JB_CJS)
 const includeESMBundle = stringToBoolean(process.env.JB_ESM_BUNDLE)
 const includeNPM = stringToBoolean(process.env.JB_NPM)
 
-export default createRollupConfig(globals, {
+const configs = createRollupConfig(globals, {
   includeUMD,
   includeCJS,
   includeESMBundle,
   includeNPM,
 })
+
+// Override the global name for UMD builds
+const processedConfigs = Array.isArray(configs) ? configs : [configs]
+processedConfigs.forEach(config => {
+  if (config.output) {
+    if (Array.isArray(config.output)) {
+      config.output.forEach(output => {
+        if (output.format === 'umd') {
+          output.name = 'JBrowsePluginImageGalleryPlugin'
+        }
+      })
+    } else if (config.output.format === 'umd') {
+      config.output.name = 'JBrowsePluginImageGalleryPlugin'
+    }
+  }
+})
+
+export default configs
