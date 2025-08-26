d31469ba0c57056ecac952fd4e65b9d1cb0af000
Alexie Papanicolaou <alpapan@gmail.com>
2025-08-26 18:43:33 +1000

    register plugin

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import Plugin from '@jbrowse/core/Plugin'
 import PluginManager from '@jbrowse/core/PluginManager'
 import ViewType from '@jbrowse/core/pluggableElementTypes/ViewType'
+import WidgetType from '@jbrowse/core/pluggableElementTypes/WidgetType'
 import { AbstractSessionModel, isAbstractMenuManager } from '@jbrowse/core/util'
 import { version } from '../package.json'
 import {
   ReactComponent as HelloViewReactComponent,
   stateModel as helloViewStateModel,
 } from './HelloView'
+import { ImageGalleryWidgetF } from './ImageGalleryWidget'
 
 export default class ImageGalleryPlugin extends Plugin {
   name = 'ImageGalleryPlugin'
@@ -19,6 +21,10 @@ export default class ImageGalleryPlugin extends Plugin {
         stateModel: helloViewStateModel,
         ReactComponent: HelloViewReactComponent,
       })
+    })
+
+    pluginManager.addWidgetType(() => {
+      return new WidgetType(ImageGalleryWidgetF(pluginManager))
     })
   }
 
